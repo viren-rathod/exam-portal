f4555c4f0157d530a877e43d665ac6ca11d66178
Viren.Rathod <virenrth@hotmail.com>
2024-05-12 12:47:40 +0530

    Added Message and Toasts

--- src/main/java/org/examportal/Constants/Exam/CategoryMessages.java
+++ src/main/java/org/examportal/Constants/Exam/CategoryMessages.java
@@ -1,5 +1,8 @@
 package org.examportal.Constants.Exam;
 
 public class CategoryMessages {
+    public static final String CATEGORY_FETCHED = "Category fetched successfully!";
+    public static final String CATEGORY_CREATED = "Category created successfully!";
+    public static final String CATEGORY_UPDATED = "Category updated successfully!";
     public static final String CATEGORY_DELETED = "Category deleted successfully!";
 }
--- src/main/java/org/examportal/Constants/Exam/ExamMessages.java
+++ src/main/java/org/examportal/Constants/Exam/ExamMessages.java
@@ -1,5 +1,10 @@
 package org.examportal.Constants.Exam;
 
 public class ExamMessages {
+    public static final String EXAM_FETCHED = "Exam fetched successfully!";
     public static final String EXAM_DELETED = "Exam deleted successfully!";
+    public static final String EXAM_CREATED = "Exam created successfully!";
+    public static final String EXAM_UPDATED = "Exam updated successfully!";
+    public static final String EXAM_STARTED = "Exam started!";
+    public static final String EXAM_STOPPED = "Exam stopped!";
 }
